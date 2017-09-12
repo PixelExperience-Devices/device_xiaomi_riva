045a6919e1898d61878c16b9877a9b6b99fbaba5
LuK1337 <priv.luk@gmail.com>
2017-09-12 16:31:23 +0000

    rolex: Add libc hax for camera
     * See : https://review.lineageos.org/#/c/1170/
    
    Change-Id: I040c3023f81322140d153cb14be9578457777ac2

--- device.mk
+++ device.mk
@@ -254,6 +254,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshims_camera
+ 
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8937
