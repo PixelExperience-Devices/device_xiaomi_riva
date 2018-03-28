fb7bf0bf6dfc06b0a3453436d55f6761db172396
Sean Tranchetti <stranche@codeaurora.org>
2018-03-28 09:40:19 -0600

    riva: change data properties to comply with Treble
    Treble changes in Android now require stricter naming conventions for
    vendor properties, and also impose stricter requirements on using
    properties as actionable triggers in init scripts. This change
    updates the properties used by data modules to follow these new
    guidelines.
    
    Change-Id: I889da9b0d0cf8eb894075649aa57dc6672ce9252
    CRs-Fixed: 2215428
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -168,9 +168,9 @@ ro.am.reschedule_service=true
 
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.use_data_netmgrd=true \
+ro.vendor.use_data_netmgrd=true \
 persist.data.netmgrd.qos.enable=true \
-persist.data.mode=concurrent
+persist.vendor.data.mode=concurrent
 
 # Nitz
 PRODUCT_PROPERTY_OVERRIDES += \
