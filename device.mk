9f4a9c4ac3bb4e0593add076fdae30ee9282ee6b
muralivijay <muralidharan9845@gmail.com>
2018-04-10 19:51:46 +0530

    Revert "santoni: Build QTI HIDL power 1.0 HAL"
    This reverts commit b136542e46dfe217eaca2685fac44e965ed77b48.

--- device.mk
+++ device.mk
@@ -294,7 +294,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    power.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
