888ddec66ee276163df32b6628b399f8ebe3e6ea
muralivijay <muralidharan9845@gmail.com>
2018-04-10 14:32:09 +0530

    rolex: Cleanup powerHAL

--- device.mk
+++ device.mk
@@ -294,10 +294,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    power.msm8937
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
