56197e8bbe982fe66bb082631ded103a8eb5f5ea
Bruno Martins <bgcngm@gmail.com>
2018-04-12 11:13:39 +0100

    rolex: Switch to HIDL ANT+ HAL
    Change-Id: I0e0252b6e2c7e3e7da1e8508f010b7e6c2bfd128

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,8 +47,8 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CONFIG := rolex_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8917
 
-# ANT
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
