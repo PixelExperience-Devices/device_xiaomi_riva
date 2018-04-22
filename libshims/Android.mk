45f5596c3b2c944180f304b8d357be50219b6152
haran <harangovindan@gmail.com>
2018-04-22 16:59:42 +0200

    rolex: Backport libc hax
    * Backport libc hax for our camera hal

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,5 +1,5 @@
-
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2013-2016, The CyanogenMod Project
+# Copyright (C) 2018, The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,9 +19,12 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     bionic/bionic_time_conversions.cpp \
     bionic/pthread_cond.cpp
-
 LOCAL_SHARED_LIBRARIES := libc
-LOCAL_MODULE := libshims_camera
+LOCAL_MODULE := libshim_camera
+LOCAL_VENDOR_MODULE := true
+LOCAL_CXX_STL := none
+LOCAL_SANITIZE := never
 LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
