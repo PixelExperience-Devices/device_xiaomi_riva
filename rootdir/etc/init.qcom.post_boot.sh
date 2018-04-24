af22986dd3f3a3b53dc8ff668ed581840c8ff6f4
muralivijay <muralidharan9845@gmail.com>
2018-04-24 18:36:34 +0530

    rolex:led: Set permissions all LED blinking PATH

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -2394,3 +2394,7 @@ case "$console_config" in
         echo "Enable console config to $console_config"
         ;;
 esac
+
+# Set Permissions For LED
+chown system:system /sys/class/leds/*/brightness
+chown system:system /sys/class/leds/*/blink
