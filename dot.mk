a23a687a8c81cd6e80ba474756cc9386e5b65245
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-06-08 01:17:12 +0000

    Dot-ify

--- dot.mk
+++ dot.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, device/xiaomi/riva/device.mk)
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := dot_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
