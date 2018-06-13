50c98a86e2048a6062b9470866fbcf51a9cc80e7
muralivijay <muralidharan9845@gmail.com>
2018-06-14 00:09:18 +0530

    rolex: drop useless compatibility_matrix.xml & manifest
    fb :D

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,10 +161,6 @@ USING_DEVICE_GPS := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
-# HIDL
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
-
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_rolex
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
