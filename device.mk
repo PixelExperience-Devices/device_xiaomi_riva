ef8e8de6f0ccfa1cc20538a8a205fcae97afa973
muralivijay <muralidharan9845@gmail.com>
2018-06-14 19:42:50 +0530

    rolex: add some memtrack packages

--- device.mk
+++ device.mk
@@ -186,7 +186,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl 
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service 
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
