eb4545d646f39e642e0325eb3dd675910ed69514
LuK1337 <priv.luk@gmail.com>
2018-07-01 14:41:43 +0200

    riva: Remove unnecessary languages_full.mk include
    * It's already included in full_base_telephony.
    
    Change-Id: I7547ff447e81641e74fd911a12f5a9f0df9e5ccf

--- device.mk
+++ device.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 $(call inherit-product, vendor/xiaomi/riva/riva-vendor.mk)
 
 # Overlays
