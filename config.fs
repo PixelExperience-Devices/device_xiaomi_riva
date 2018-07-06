9289102def47605ff4da8a4343999fcc3c993f42
muralivijay <muralidharan9845@gmail.com>
2018-07-07 00:12:42 +0530

    rolex: vendorize  config.fs and && cleanup

--- config.fs
+++ config.fs
@@ -13,7 +13,7 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
 
-[system/vendor/bin/wcnss_filter]
+[vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
@@ -25,25 +25,19 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
-[system/bin/cnss-daemon]
+[vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: NET_BIND_SERVICE
 
 [vendor/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -61,12 +55,6 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/pd-mapper]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [system/bin/pd-mapper]
 mode: 0755
 user: AID_SYSTEM
@@ -79,7 +67,7 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -91,7 +79,7 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
@@ -103,7 +91,7 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: WAKE_ALARM
 
-[system/vendor/bin/imsrcsd]
+[vendor/bin/imsrcsd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
@@ -115,19 +103,13 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[system/vendor/bin/cnd]
+[vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/slim_daemon]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/slim_daemon]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
