86727327f9b2917d1e70aa8b4c0d42ce3637ff36
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-07-15 02:09:24 +0000

    riva: update build fingerprint from miui

--- lineage_riva.mk
+++ lineage_riva.mk
@@ -25,8 +25,8 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="riva-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
+    PRIVATE_BUILD_DESC="riva-user 7.1.2 N2G47H V9.5.6.0.NCKMIFA release-keys"
 
 # Set BUILD_FINGERPRINT variable
-BUILD_FINGERPRINT := "Xiaomi/riva/riva:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/riva/riva:7.1.2/N2G47H/V9.5.6.0.NCKMIFA:user/release-keys"
 
