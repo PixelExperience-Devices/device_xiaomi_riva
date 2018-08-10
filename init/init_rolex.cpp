7ffcf971094c25a532527ca759ccef5ee4fa2df8
Ritesh Saxena <riteshsax007@gmail.com>
2018-08-10 15:04:22 +0000

    rolex: init: Cleanup
    Change-Id: Ifc37262d00959ea51d9f1b8c39492363e3ad2183

--- init/init_rolex.cpp
+++ init/init_rolex.cpp
@@ -32,14 +32,11 @@
 #include <string>
 #include <sstream>
 #include <sys/sysinfo.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
 
 #include <android-base/properties.h>
 #include <android-base/logging.h>
 #include "vendor_init.h"
 #include "property_service.h"
-#include "util.h"
 
 char const *heapstartsize;
 char const *heapgrowthlimit;
@@ -48,19 +45,7 @@ char const *heapminfree;
 char const *heapmaxfree;
 char const *large_cache_height;
 
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-static void init_alarm_boot_properties()
-{
+static void init_alarm_boot_properties(){
     int boot_reason;
     FILE *fp;
 
@@ -123,8 +108,7 @@ void check_device()
    }
 }
 
-void vendor_load_properties()
-{
+void vendor_load_properties(){
     init_alarm_boot_properties();
     check_device();
 
