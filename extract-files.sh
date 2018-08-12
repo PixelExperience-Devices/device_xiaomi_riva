8cfbc9ca8daf254fd1e39490875273455fb634e0
LuK1337 <priv.luk@gmail.com>
2018-08-12 16:10:01 +0200

    rolex: Vndk-hack
    Change-Id: I304cbfbfba8c7a4c403a8e865bf0d940cac1bea2

--- extract-files.sh
+++ extract-files.sh
@@ -67,4 +67,8 @@ CAMERA2_SENSOR_MODULES="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/ve
 
 sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
 
+COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+
+patchelf --replace-needed android.hardware.gnss@1.0.so android.hardware.gnss@1.0-v27.so $COMMON_BLOB_ROOT/vendor/lib64/vendor.qti.gnss@1.0_vendor.so
+
 "$MY_DIR"/setup-makefiles.sh
