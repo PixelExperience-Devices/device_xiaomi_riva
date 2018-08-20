6e13a24cc8b076cd78e61b617943139b5db2a143
haran2003 <harangovindan@outlook.com>
2018-08-20 10:47:14 +0200

    rolex: init: Include libselinux

--- init/Android.mk
+++ init/Android.mk
@@ -20,10 +20,13 @@ include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
     system/core/base/include \
-    system/core/init
+    system/core/init \
+    external/selinux/libselinux/include
 LOCAL_MODULE := libinit_rolex
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_rolex.cpp
-LOCAL_STATIC_LIBRARIES := libbase
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libselinux
 
 include $(BUILD_STATIC_LIBRARY)
