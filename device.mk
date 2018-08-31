a7fd4581261d2e019ea8197fcce9e4287b4822c1
muralivijay <muralidharan9845@gmail.com>
2018-09-01 03:26:50 +0530

    rolex: update dalvik vm heapsize
    from : Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.6.3.0.NCCMIFD:user/release-keys

--- device.mk
+++ device.mk
@@ -197,7 +197,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapsize=384m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heaptargetutilization=0.75
 
