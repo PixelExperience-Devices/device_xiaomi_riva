43b41481fd2b92bdf16598bd916af5fb3b05f328
muralivijay <muralidharan9845@gmail.com>
2018-09-07 19:13:45 +0000

    Revert "riva: drop lineage hardware support"
    This reverts commit 1019669edd90f855600bd1aaf5e46ac99d702d51.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,6 +194,10 @@ $(shell rm -rf hardware/qcom/display-caf/msm8996/liblight)
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(DEVICE_PATH)/lineagehw
+
 # Tap2Wake
 TARGET_TAP_TO_WAKE_NODE := "/proc/gesture/onoff"
 
