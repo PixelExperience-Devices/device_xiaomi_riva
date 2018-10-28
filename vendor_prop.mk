066c065f4979beaf6f823ec895f8973670a4e9bf
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-28 16:18:47 +0700

    riva: remove old gps Prop
    * Remove "com.qualcomm.location"
    * Switch to "com.google.android.gms"
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,9 +132,8 @@ ro.max.fling_velocity=20000
 
 # GPS
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
+ro.location.osnlp.package=com.google.android.gms \
+ro.location.osnlp.region.package=
 
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
