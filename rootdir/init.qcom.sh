a54b11c3d24b2b0783f1dbe4582dd52d69f58e77
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-12-17 00:28:29 +0000

    Revert "riva: Add ims hex"
    This reverts commit 15a53257dbc0b1a5f2b15f723931933b73bcebe2.

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -385,11 +385,6 @@ case "$buildvariant" in
         ;;
 esac
 
-if [ -f /data/system/users/0/settings_global.xml ]; then
-    sed -i 's/"multi_sim_data_call" value="1"/"multi_sim_data_call" value="-1"/g' /data/system/users/0/settings_global.xml
-    restorecon /data/system/users/0/settings_global.xml
-fi
-
 on post-fs-data
 
     # IR nodes
