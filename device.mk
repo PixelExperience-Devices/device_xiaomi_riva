e2f91e7a5b7a3354f27f97f724c073681d8960b3
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-02-22 05:39:16 +0000

    Drop AW2013 light hal

--- device.mk
+++ device.mk
@@ -256,7 +256,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_riva
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_COPY_FILES += \
