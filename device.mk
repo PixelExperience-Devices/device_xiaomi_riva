4a46156e15de0a1f1a0e09fb54efee1c51846ff0
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    riva: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 
 DEVICE_PATH := device/xiaomi/riva
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
