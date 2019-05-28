2ac12ce2b867751cc8cc2a83e673a50d0fd49e69
Felix <google@ix5.org>
2019-05-28 14:05:51 +0200

    riva: Use BOARD_VENDOR_SEPOLICY_DIRS
    BOARD_SEPOLICY_DIRS is deprecated.
    
    Change-Id: I046282b2a2e8c541726fb29cb0044503322d4be9
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,7 +192,7 @@ VENDOR_SECURITY_PATCH := 2020-06-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
