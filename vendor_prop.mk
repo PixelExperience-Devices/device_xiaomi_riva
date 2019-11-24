3aac90371737b74f4238ff91614d612580c1ad12
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-11-24 15:08:34 +0530

    riva: Enabled 1080p on youtube

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -113,7 +113,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_rotator_split=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.perf_hint_window=50 \
-    vendor.gralloc.enable_fb_ubwc=1
+    vendor.gralloc.enable_fb_ubwc=1 \
+    sys.display-size=1920x1080
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
