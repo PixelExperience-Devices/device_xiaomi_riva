43b22fee94fd2298d4f18ecb0d172c35fab7135f
Ahmad Rulim <mfalkia342@gmail.com>
2019-12-27 13:01:36 +0700

    Revert "riva: Adding GSI keys"
    This reverts commit e0be96e921287505d9f858eb99402bc94c2c54e8.

--- device.mk
+++ device.mk
@@ -17,9 +17,6 @@
 
 DEVICE_PATH := device/xiaomi/riva
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
