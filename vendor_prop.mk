d8a2339c50239207ad79c6bb8ce47b10ce00b162
FauzanDhiyaul <fdhiyaul163@gmail.com>
2020-01-02 13:33:33 +0700

    riva: Force enable IMS/Radio props
    * Fix VoLTE
    
    Signed-off-by: FauzanDhiyaul <fdhiyaul163@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -172,6 +172,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.data.mode=concurrent
 
+# Radio Debug Options
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
+
+# Radio Options
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true \
