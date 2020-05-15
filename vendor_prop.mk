b03cac7f453bcb267eb781b66c934fd7e8d5badc
Anusha Shakarad <ashaka@codeaurora.org>
2020-05-15 19:11:24 +0200

    riva: Disable SF backpressure
    Change-Id: I3bacf63cdeab50be41306e8d58eed1c0d9316258
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,6 +106,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.enable.sglscale=1 \
     debug.mdpcomp.logs=0 \
     debug.sdm.support_writeback=0 \
+    debug.sf.disable_backpressure=1 \
     debug.sf.hw=0 \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
