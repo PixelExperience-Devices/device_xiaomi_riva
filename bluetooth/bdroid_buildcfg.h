ec5c3290d0f09b82a1dc4477d17090c40db19f57
Nick C <ctwoon@suicide.today>
2020-05-30 12:50:04 +0300

    riva: Update bdroid_buildcfg values from caf
    https: //source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/bluetooth-commonsys-intf/tree/build/qva/config/bdroid_buildcfg.h?h=LA.UM.8.6.2.r1-08400-89xx.0
    
    Change-Id: I5a95bec3db32eb4f6f83f33087612cf43294fc5b
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,19 +18,21 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    7
-#define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
-#define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED 1
-#define BTA_DISABLE_DELAY 1000
-#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-#define BTM_WBS_INCLUDED TRUE
+#define BTM_DEF_LOCAL_NAME "Redmi 5A"
+#define MAX_ACL_CONNECTIONS 16
+#define MAX_L2CAP_CHANNELS 32
+#define BLE_VND_INCLUDED TRUE
+#define GATT_MAX_PHY_CHANNEL 10
+
+// Wide-band speech support
 #define BTIF_HF_WBS_PREFERRED TRUE
 
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS                  12
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
-#endif
+/* Disable enhanced sco commands */
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+
+#define AVDT_NUM_SEPS 35
+
+#endif /* _BDROID_BUILDCFG_H */
