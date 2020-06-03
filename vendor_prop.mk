6e98c957c66738ecc108e4b217151df86489900d
Nick C <ctwoon@suicide.today>
2020-06-04 00:33:57 +0300

    riva: props: enable zygote preforking
    * faster app launching

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -161,3 +161,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
