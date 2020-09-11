2545b37d8d6b92d2677f8565abbbbaab9bd2bf65
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    riva: Copy VNDK-29 libbinderthreadstate.so to vendor
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- device.mk
+++ device.mk
@@ -409,7 +409,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# WiFi
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
+# Wifi
 PRODUCT_PACKAGES += \
     libwpa_client \
 
