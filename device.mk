82d5fe83b4463f85eb0793efdee9e0498d2eabfd
Ahmad Rulim <mfalkia342@gmail.com>
2020-09-20 23:00:45 +0700

    riva: Build libhwbinder/libhidltransport
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- device.mk
+++ device.mk
@@ -399,17 +399,20 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
-# VNDK
-PRODUCT_PACKAGES += \
-    libstdc++.vendor \
-    libwui
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libstdc++.vendor \
+    libwui \
+    libhwbinder \
+    libhwbinder.vendor \
+    libhidltransport \
+    libhidltransport.vendor
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so \
