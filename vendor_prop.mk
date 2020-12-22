c7372f0a37cf3d4c42cb8cf5ac7feb50fadc6148
Ahmad Rulim <mfalkia342@gmail.com>
2020-12-22 10:03:44 +0700

    riva: Drop wfc
    Unsupported by the device
    
    Signed-off-by: Ahmad Rulim <mfalkia342@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -124,7 +124,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
